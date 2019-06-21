8c3693dd05194f0df4e8448d604082d03166a5ee
leets <lucidesp@gmail.com>
2019-06-21 17:17:42 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// 깃 저장소 
+//  https://github.com/taisunglee/sem.git
+function App() { 
+  return (
+    <div className="App">
+      <header className="App-header">
+        안녕하세요.
+      </header>
+    </div>
+  );
+}
+
+export default App;
